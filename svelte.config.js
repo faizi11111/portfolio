7cc80bb66352f1ed7eb0311640e15bf9382a6df3
faizi11111 <faizanfarooq993@gmail.com>
2024-05-04 03:17:16 +0500

    initialized the project. added tailwindcss and fonts

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+// @ts-ignore
+import adapter from '@sveltejs/adapter-auto';
+// @ts-ignore
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
+};
+export default config;
